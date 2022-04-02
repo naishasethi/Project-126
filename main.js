4a6d8ca852dddc59c69d3315eaabecad6e63dee2
naishasethi <79583662+naishasethi@users.noreply.github.com>
2022-04-02 18:55:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-song1=""
-song2=""
+song1="";
+song2="";
+leftWristx=0;
+leftWristy=0;
+rightWristx=0;
+rightWristy=0;
 function preload() {
     song=loadSound("music.mp3");
     song=loadSound("music2.mp3");
@@ -9,6 +13,22 @@ function setup() {
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Posenet is Loaded");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristx=results[0].pose.leftWrist.x;
+        leftWristy=results[0].pose.leftWrist.y;
+        console.log("Left Wrist x =" + leftWristx + "  Left Wrist y=" + leftWristy);
+        rightWristx=results[0].pose.rightWrist.x;
+        rightWristy=results[0].pose.rightWrist.y;
+        console.log("Right Wrist x =" + rightWristx + "  Right Wrist y=" + rightWristy);
+    }
 }
 function draw() {
     image(video, 0, 0, 600, 400);
