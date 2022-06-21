c9d9edc2275373b5e804c9453c3ef652bb75d9e1
naishasethi <79583662+naishasethi@users.noreply.github.com>
2022-06-21 16:51:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ song2status="";
 scoreRightWrist=0;
 scoreLeftWrist=0;
 function preload() {
-    song=loadSound("music.mp3");
-    song=loadSound("music2.mp3");
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
 }
 function setup() {
     canvas=createCanvas(600,400);
